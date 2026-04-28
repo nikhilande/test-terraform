d3b5965fb13f7c4ff0c545b6e03e9e9b26e5f452
Nikhil Ande <69084565+nikhilande@users.noreply.github.com>
2026-04-28 17:27:20 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "db" {
   engine            = "mysql"
   instance_class    = var.instance_class
 
-  username = "admin"
-  password = "password123"
+  username = var.db_username
+  password = var.db_password
 
   skip_final_snapshot = true
 }
